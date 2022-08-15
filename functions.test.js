02b378cc0d7cd2e70a768b96cebdef25bb2f60c4
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2022-08-16 00:04:45 +0530

    Non Premitive Matchers

--- functions.test.js
+++ functions.test.js
@@ -1,9 +1,61 @@
 const functions = require('./functions')
 
+// toBe
 test('Add 2 + 2 to equal 4', () => {
   expect(functions.add(2, 2)).toBe(4)
 })
 
+// not toBe
 test('Add 2 + 2 to NOT equal 5', () => {
   expect(functions.add(2, 2)).not.toBe(5)
+})
+
+// Check for Truthy & Falsy Values
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// tobeTruthy matches anything that an if statement treats as true
+// tobeFalsy matches anything that an if statement treats as false
+
+// toBeNull
+test('Should be null', () => {
+  expect(functions.isNull()).toBeNull()
+})
+
+// toBeFalsy
+test('Should be falsy', () => {
+  expect(functions.checkValue(null)).toBeFalsy()
+  expect(functions.checkValue(undefined)).toBeFalsy()
+})
+
+// toBeTruthy
+// test('Should be truthy', () => {
+//   expect(functions.checkValue(2)).toBeTruthy()
+// })
+
+// toEqual
+test('User should be Brad Traversy object', () => {
+  expect(functions.createUser()).toEqual({
+    firstName: 'Brad',
+    lastName: 'Traversy',
+  })
+})
+
+// Less than and greater than
+test('Should be under 1600', () => {
+  const load1 = 800
+  const load2 = 800
+  // expect(load1 + load2).toBeLessThan(1600)
+  expect(load1 + load2).toBeLessThanOrEqual(1600)
+})
+
+// Regex
+test('There is no I in team', () => {
+  expect('team').not.toMatch(/I/)
+})
+
+// Arrays
+test('Admin should be in usernames', () => {
+  const usernames = ['john', 'karen', 'admin']
+  expect(usernames).toContain('admin')
 })
