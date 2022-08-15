d3c41001606a5128b266c5e57736cc1a6567854f
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2022-08-16 00:16:40 +0530

    Async Data

--- functions.test.js
+++ functions.test.js
@@ -59,3 +59,20 @@ test('Admin should be in usernames', () => {
   const usernames = ['john', 'karen', 'admin']
   expect(usernames).toContain('admin')
 })
+
+// Working with async data
+
+// Promise
+// test('User fetched name should be Leanne Graham', () => {
+//   expect.assertions(1)
+//   return functions.fetchUser().then((data) => {
+//     expect(data.name).toEqual('Leanne Graham')
+//   })
+// })
+
+// Async Await
+test('User fetched name should be Leanne Graham', async () => {
+  expect.assertions(1)
+  const data = await functions.fetchUser()
+  expect(data.name).toEqual('Leanne Graham')
+})
